aff773d7d12162e2397d65426cc7a53f3ea8f9ca
Christia T <christiatidwell@bellsouth.net>
2018-06-11 01:01:21 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,7 @@ def kind
     raise TriangleError
   elsif side1 == 0 || side2 == 0 || side3 ==0
    raise TriangleError
-  elsif side1 == side2 == side3
+ elsif side1 == side2 && side2 == side3
     return :equilateral
   elsif side1 == side2 || side2 == side3 || side1 == side3
     return :isosceles
@@ -29,7 +29,7 @@ def kind
  end
 
  class TriangleError < StandardError
-    
+
    end
- 
+
 end
