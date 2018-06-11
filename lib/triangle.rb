01e8f4745bdeb681204ba329acbde7db2ab01d63
Christia T <christiatidwell@bellsouth.net>
2018-06-11 01:02:55 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,11 +11,11 @@ end
 
 def kind
 
-  if side1 + side2 < side3
+  if side1 + side2 <= side3
     raise TriangleError
-  elsif side2 + side3 < side1
+  elsif side2 + side3 <= side1
     raise TriangleError
-  elsif side1 + side3 < side2
+  elsif side1 + side3 <= side2
     raise TriangleError
   elsif side1 == 0 || side2 == 0 || side3 ==0
    raise TriangleError
