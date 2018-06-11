22769260a2fc22c0253f69e1c6e9f34f58b595b0
Christia T <christiatidwell@bellsouth.net>
2018-06-11 00:50:15 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,20 @@
 class Triangle
 
+attr_accessor :side1, :side2, :side3
+
 def initialize(side1, side2, side3)
-  @sides = []
-  @sides << side1
-  @sides << side2
-  @sides << side3
+  @side1 = side1
+  @side2 = side2
+  @side3 = side3
 end
 
 
 def kind
+
+  if side1 + side2 > side3 && @sides.length.all > 0
+    "valid"
+  else
+   raise TriangleError
   elsif side1 == side2 == side3
     return :equilateral
   elsif side1 == side2 || side2 == side3 || side1 == side3
@@ -20,10 +26,7 @@ end
 
 
 
-  if @sides[0] + @sides[1] > @sides[2] && @sides.length.all > 0
-    "valid"
-  else
-   raise TriangleError
+
   end
  end
 
